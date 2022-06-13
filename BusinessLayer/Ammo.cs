df7f93f1191262fe7fa9d2e121720c8eadfda345
VNK4 <ivan.ignatov2504@gmail.com>
2022-06-13 13:38:51 +0300

    Initial commit

---
+++ BusinessLayer/Ammo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Ammo
+    {
+        [Key]
+        
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public double Damage { get; set; }
+
+        [Required]
+        public Enum DamageType { get; set; }
+
+        public List<Weapon> Weapons { get; set; }
+
+        private Ammo()
+        {
+            
+        }
+
+        public Ammo(string name, string description, double damage, Enum damageType)
+        {
+            Name = name;
+            Description = description;
+            Damage = damage;
+            DamageType = damageType;
+        }
+    }
+}
---
+++ BusinessLayer/Weapon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Weapon
+    {
+        [Key]
+        public int ID { get; private set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public float DamageMultiplier { get; set; }
+
+        [Required]
+        public Ammo Ammo { get; set; }
+
+        public List<Enemy> Enemies { get; set; }
+
+        private Weapon()
+        {
+            
+        }
+
+        public Weapon(string name, string description, float damageMultiplier, Ammo ammo)
+        {
+            Name = name;
+            Description = description;
+            DamageMultiplier = damageMultiplier;
+            Ammo = ammo;
+        }
+    }
+}
